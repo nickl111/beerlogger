3fc225c5bf871fb7b030a7b31e44eee094fa0b92
Nick Lott <nickl@fastmail.com>
2018-07-31 08:32:02 +0100

    Minor pin change

--- core/config.sh
+++ core/config.sh
@@ -5,7 +5,7 @@
 IN=22		# Pin to monitor the interrupt
 LED_R=27	# Pin that drives the Red LED
 LED_G=17	# Pin that drives the Green LED
-LED_O=25	# Pin that drives the Orange LED
+LED_O=10	# Pin that drives the Orange LED
 OW=4	#1-wire pin. We don't reference it anywhere but just a reminder not to use it
 
 # The two temp sensor 1-wire addresses. T1 is the remote, T2 is the home
