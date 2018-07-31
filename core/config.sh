afafee603c182f82e31bfe7eb379b19a0b81f706
Nick Lott <nickl@fastmail.com>
2018-07-31 20:16:23 +0100

    Fix issue #3, set config to real board pins

--- core/config.sh
+++ core/config.sh
@@ -2,11 +2,11 @@
 
 # Config file
 
-IN=22		# Pin to monitor the interrupt
-LED_R=27	# Pin that drives the Red LED
-LED_G=17	# Pin that drives the Green LED
-LED_O=10	# Pin that drives the Orange LED
-OW=4	#1-wire pin. We don't reference it anywhere but just a reminder not to use it
+IN=24		# Pin to monitor the interrupt on
+LED_R=23	# Pin that drives the Red LED
+LED_G=27	# Pin that drives the Green LED
+LED_O=22	# Pin that drives the Orange LED
+OW=4		#1-wire pin. We don't reference it anywhere but just a reminder not to use it
 
 # The two temp sensor 1-wire addresses. T1 is the remote, T2 is the home
 T1_ADDR=/sys/bus/w1/devices/28-00000ab5657e/w1_slave
@@ -16,6 +16,6 @@ HOME_DIR=/usr/local/beerlog
 DATA_DIR=/usr/share/beerlog
 RUN_DIR=/var/run/beerlog		# Where the run files live
 SQL_DB=$DATA_DIR/beerlog.db
-OUTRRD=$RUN_DIR/rrd.in
+OUTRRD=/dev/shm/rrd.in
 
 OUT_DIR=/var/www/beerlog
