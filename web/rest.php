3fdff9e5ba113b1d32eaaf173907112f3b4f63d1
Nick Lott <nickl@fastmail.com>
2018-08-26 13:30:28 +0100

    Collect POST data correctly; renumber errors

--- web/rest.php
+++ web/rest.php
@@ -20,7 +20,7 @@ if(in_array($_GET['o'],$perm_objects)) {
 	$o = new $o_name($db);
 } else {
 	// unpermitted view. Should probably log an error somewhere
-	$r['meta'] = array('status' => '400' , 'errno' => '100', 'message' => 'Object does not exist');
+	$r['meta'] = array('status' => '400' , 'errno' => '1', 'message' => 'Object does not exist');
 	$do = '';
 }
 
@@ -28,7 +28,7 @@ if(in_array($_GET['do'],$perm_actions)) {
 	$do = $_GET['do'];
 } else {
 	// unpermitted action. Should probably log an error somewhere
-	$r['meta'] = array('status' => '400' , 'errno' => '101', 'message' => 'Action is not recognized');
+	$r['meta'] = array('status' => '400' , 'errno' => '2', 'message' => 'Action is not recognized');
 	$do = '';
 }
 
@@ -37,50 +37,47 @@ if($_GET['pks']) {
 	$pks = explode(',', $_GET['pks']);
 }
 
-
 switch ($do) {
 	case 'delete':
 		if($pks) {
 			if($o->load($pks)) {
 				if(!$o->destroy() ) {
-					$r['meta'] = array('status' => '500' , 'errno' => '1', 'message' => 'Failed to destroy '.get_class($o).' with PK '.implode(',',$pks));
+					$r['meta'] = array('status' => '500' , 'errno' => '3', 'message' => 'Failed to destroy '.get_class($o).' with PK '.implode(',',$pks));
 				}
 			} else { 
-				$r['meta'] = array('status' => '500' , 'errno' => '2', 'message' => 'Failed to load '.get_class($o).' with PK '.implode(',',$pks));
+				$r['meta'] = array('status' => '500' , 'errno' => '4', 'message' => 'Failed to load '.get_class($o).' with PK '.implode(',',$pks));
 			}
 		} else {
-			$r['meta'] = array('status' => '400' , 'errno' => '3', 'message' => 'pks not supplied');
+			$r['meta'] = array('status' => '400' , 'errno' => '5', 'message' => 'pks not supplied');
 		}
 		break;
 	case 'create':
 	case 'update':
 		if($pks) {
 			if(!$o->load($pks)) {
-				$r['meta'] = array('status' => '500' , 'errno' => '4', 'message' => 'Failed to load '.get_class($o).' with PK '.implode(',', $pks));
+				$r['meta'] = array('status' => '500' , 'errno' => '6', 'message' => 'Failed to load '.get_class($o).' with PK '.implode(',', $pks));
 				break;
 			}
 		}
 		foreach($_POST as $k => $v) {
-			if(substr($k,0,6) == 'field_') {
-				$o->fields[substr($k,6)] = $v;
-			}
+			$o->fields[$k] = $v;
 		}
 		
 		if(!$o->save()) {
-			$r['meta'] = array('status' => '500' , 'errno' => '5', 'message' => 'Failed to save '.get_class($o));
+			$r['meta'] = array('status' => '500' , 'errno' => '7', 'message' => 'Failed to save '.get_class($o));
 		}
 		
 		$r['data'] = $o;
 		
 		break;
 	case 'list':
-		if($_POST['where']) {
-			$w = $_POST['where'];
+		if($post_data['where']) {
+			$w = $post_data['where'];
 		} else {
 			$w = '1=1';
 		}
+		$r['data'] = array();
 		if($o->find($w)) {
-			$r['data'] = array();
 			while($o->load()) {
 				$r['data'][] = clone $o;
 			}
@@ -90,7 +87,7 @@ switch ($do) {
 		if($o->load($pks)) {
 			$r['data'] = $o;
 		} else {
-			$r['meta'] = array('status' => '500' , 'errno' => '6', 'message' => 'Failed to load '.get_class($o).' with PK '.implode(',',$pks));
+			$r['meta'] = array('status' => '500' , 'errno' => '8', 'message' => 'Failed to load '.get_class($o).' with PK '.implode(',',$pks));
 		}
 		break;
 		
